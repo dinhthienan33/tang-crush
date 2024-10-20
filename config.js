a6a6d07c1c5e1cd8dfc0cb11ab88b5be38a49133
Đinh Thiên Ân <134404922+dinhthienan33@users.noreply.github.com>
2024-10-20 23:07:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Thi à!',
+    introDesc: `Hôm nay là 20.10`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    title: 'Chúc em luôn luôn xinh đẹp nhe😙',
+    desc: ' Phải luôn giữ được sự hồn nhiên này ',
+    btnYes: 'Ân đẹp trai lắm',
+    btnNo: 'Ân xấu',
+    question:'Anh ân đẹp trai không?',
+    btnReply: 'Cảm ơn em',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Hẹn em noelll',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/dinh.an.3323' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
